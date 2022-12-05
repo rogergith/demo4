318153a640f95364925c75f9f4e01062fb358b2f
Roger Araque <raismaelr@gmail.com>
2022-12-05 06:06:44 -0400

    ss

--- src/main/java/com/example/demo/Demo4Application.java
+++ src/main/java/com/example/demo/Demo4Application.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Demo4Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo4Application.class, args);
+	}
+	
+	@GetMapping(path = "/saludo")
+	public String sendGreeting() {
+		return "holaaa";
 	}
 
 }
